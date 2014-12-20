f78275c35a3dfaa5401b43de735456edd0101031
jaseg <code@jaseg.net>
2014-12-20 19:57:41 +0100

    Names and words dicts fundamentally working (though still a bit slow)

--- names.py
+++ names.py
@@ -20,26 +20,27 @@ class EntryType(Enum):
 	organization= 12
 	oik			= 13
 
+	ENTRY_TYPES = {
+			"family or surname":							surname,
+			"place name":									place,
+			"unclassified name":							unclass,
+			"company name":									company,
+			"product name":									product,
+			"work of art, literature, music, etc. name":	work,
+			"male given name or forename":					masc,
+			"female given name or forename":				fem,
+			"full name of a particular person":				person,
+			"given name or forename, gender not specified":	given,
+			"railway station":								station,
+			"organization name":							organization,
+			"old or irregular kana form" :					oik }
+	ENTRY_DESCRIPTIONS = { v:k for k,v in ENTRY_TYPES.items() }
+
 	def __str__(self):
-		return ENTRY_TYPE_DESCRIPTIONS[self]
+		return ENTRY_DESCRIPTIONS[self]
 
 Translation	= namedtuple('Translation', ['translations', 'types'])
 Entry		= namedtuple('Entry', ['kanji', 'readings', 'translations'])
-
-ENTRY_TYPE_DESCRIPTIONS = {
-		EntryType.surname:		"family or surname",
-		EntryType.place:		"place name",
-		EntryType.unclass:		"unclassified name",
-		EntryType.company:		"company name",
-		EntryType.product:		"product name",
-		EntryType.work:			"work of art, literature, music, etc. name",
-		EntryType.masc:			"male given name or forename",
-		EntryType.fem:			"female given name or forename",
-		EntryType.person:		"full name of a particular person",
-		EntryType.given:		"given name or forename, gender not specified",
-		EntryType.station:		"railway station",
-		EntryType.organization:	"organization name",
-		EntryType.oik:			"old or irregular kana form" }
 
 if __name__ == '__main__':
 	from xml.etree.ElementTree import ElementTree
@@ -59,7 +60,7 @@ if __name__ == '__main__':
 			# strip leading & and trailing ; from entity (e.g. "&person;"→"person")
 			translations = frozenset({
 					names.Translation(tuple( det.text for det in tr.findall('trans_det') ),
-								tuple( nt.text for nt in tr.findall('name_type') ))
+								tuple( ENTRY_TYPES[nt.text] for nt in tr.findall('name_type') ))
 					for tr in trans })
 			return names.Entry(kanji, readings, translations)
 		entries = { parse_entry(entry) for entry in tree.getroot().findall('entry') }
